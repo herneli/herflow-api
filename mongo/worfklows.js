ae055c9c38be3ddc3fd6edebb54211a8fe42415c
Jordi Hernandez <jordi.hernandez@gmail.com>
2017-12-14 22:54:35 +0100

    Initial commit

---
+++ mongo/worfklows.js
@@ -0,0 +1,23 @@
+let db = [
+  {
+    name: "Worklfow de test 1",
+    createdBy: "hernaj34",
+    tasks: [
+      { name: "W1 - Tarea 1" , status: "open", assignedTo: "hernaj34"},
+      { name: "W1 - Tarea 2" , status: "close", assignedTo: "sanzcans"},
+      { name: "W1 - Tarea 3" , status: "open", assignedTo: "hernaj34"}
+    ]
+  },
+  {
+    name: "Worklfow de test 2",
+    createdBy: "hernaj34",
+    tasks: [
+      { name: "W2 - Tarea 1" , status: "open", assignedTo: "hernaj34"},
+      { name: "W2 - Tarea 2" , status: "close", assignedTo: "sanzcans"},
+      { name: "W2 - Tarea 3" , status: "close", assignedTo: "hernaj34"}
+    ],
+    workflow: {
+      id: 
+    }
+  }
+]  
